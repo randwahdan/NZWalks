9dc23506e4761113450bc4be8451d498b5c43215
RandDev <80163738+randwahdan@users.noreply.github.com>
2024-06-28 10:35:51 +0300

    Create Domain Models And Migration

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https//:localhost:portNumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET : https//:localhost:portNumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents() 
+        {
+            string[] studentsName = new string[] { "Rand", "Abdulrahman", "Rahaf", "Roaa" };
+            return Ok();    
+        }
+
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//inject our DbContext class and provided ConnectionString to DbContext
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{ 
+options.UseSqlServer("Data Source=localhost;Initial Catalog=NZWalksDb;Integrated Security=True;TrustServerCertificate=True;");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
