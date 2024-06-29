6dc6745461d19170dbce7ac4e9c699360b14e396
RandDev <80163738+randwahdan@users.noreply.github.com>
2024-06-29 23:11:24 +0300

    Implement Automapper

---
+++ NZWalks.API/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Mappings
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //take resource and destination
+            CreateMap<Region, RegionDTO>().ReverseMap(); 
+            CreateMap<AddRegionRequestDTO, Region>().ReverseMap();
+            CreateMap<UpdateRegionRequestDTO, Region>().ReverseMap();
+
+        }
+    }
+}
